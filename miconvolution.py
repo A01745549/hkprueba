0796f89bcd7c23dd96b419c67a250c33c929b5b9
A01745549 <69917305+A01745549@users.noreply.github.com>
2020-10-28 14:50:23 -0600

    Create miconvolution.py

---
+++ miconvolution.py
@@ -0,0 +1,34 @@
+git add
+git ls
+touch miconvolution.py
+ls
+git status
+git add miconvolution.py
+git commit -m "hola"
+vim mi convolucion.py
+#funcion que calcula la matriz resultante C despues de aplicar la operacion convolucion de a*b
+
+def convolucion()
+C=0
+return C
+matriz1=[[6,9,0,3].[8,4,9,1],[4,1,3,12],[3,2,1,100]]
+filtro=[[1,0,2],[5,0,9],[6,2,1]]
+esc:wq
+python3 miconvolucion.py
+pip3 install numpy 
+python3
+import numpy 
+import numpy as np
+def convolucion()
+C=0
+return C
+matriz1=[[6,9,0,3].[8,4,9,1],[4,1,3,12],[3,2,1,100]]
+filtro=[[1,0,2],[5,0,9],[6,2,1]]
+A=np.array(matriz1)
+B=np.array(filtro)
+
+print(A[1],[0])
+print(C)
+print(A)
+esq:wq
+
